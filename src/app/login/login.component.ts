270331140295fb35b5ade45b2e61644d32d6a81f
anitamatyas <anita@camelcoding.com>
2024-03-25 09:35:58 +0200

    login page changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isStudent: boolean = true;
+  isSignUp: boolean = false;
 
+  toggleRole(): void {
+    this.isStudent = !this.isStudent;
+    this.isSignUp = false;
+  }
+
+  toggleView(): void {
+    this.isSignUp = !this.isSignUp;
+  }
 }
